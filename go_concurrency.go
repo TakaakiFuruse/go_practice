e043963b67d0071fcdccc39f35914252bbeb5407
TakaakiFuruse <takaakifuruse@gmail.com>
2015-11-18 14:54:50 +0900

    concurrency

--- go_concurrency.go
+++ go_concurrency.go
@@ -80,3 +80,44 @@ func main() {
 	waitGroup.Wait()
 	fmt.Println(time.Now())
 }
+
+//-------------------------------------------
+
+package main
+
+import (
+  "fmt"
+  "log"
+  "time"
+)
+
+func wait1(c chan string) {
+  time.Sleep(1 * time.Second)
+  log.Print("waited 1 sec")
+  c <- "wait 1 finished"
+
+}
+
+func wait2(c chan string) {
+  time.Sleep(2 * time.Second)
+  log.Print("waited 2 sec")
+  c <- "wait 2 finished"
+}
+
+func wait3(c chan string) {
+  time.Sleep(3 * time.Second)
+  log.Print("waited 3 sec")
+  c <- "wait 3 finished"
+}
+
+func main() {
+  c := make(chan string)
+  log.Print("started")
+  go wait1(c)
+  go wait2(c)
+  go wait3(c)
+  w1, w2, w3 := <-c, <-c, <-c
+  log.Print("finished")
+  fmt.Println(w1, w2, w3)
+}
+
