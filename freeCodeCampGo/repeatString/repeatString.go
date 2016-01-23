913a4db92ac5f9501955191102cf7341bf8c5997
TakaakiFuruse <takaakifuruse@gmail.com>
2016-01-23 18:36:49 +0900

    done

--- freeCodeCampGo/repeatString/repeatString.go
+++ freeCodeCampGo/repeatString/repeatString.go
@@ -4,3 +4,12 @@ type Repeat struct {
 	words string
 	num   int
 }
+
+func (r Repeat) repeat() string {
+	var s string
+
+	for i := 1; i <= r.num; i++ {
+		s += r.words
+	}
+	return s
+}
--- freeCodeCampGo/repeatString/repeatString_test.go
+++ freeCodeCampGo/repeatString/repeatString_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 func Test(t *testing.T) {
-  assert.Equal(t, Repeat(words: "*", num: 3).repeat, "***", "1")
-	assert.Equal(t, Repeat(words: "abc", num: 3).repeat, "abcabcabc", "2")
-	assert.Equal(t, Repeat(words: "abc", num: 4).repeat, "abcabcabcabc", "3")
-	assert.Equal(t, Repeat(words: "abc", num: 1).repeat, "abc", "4")
-	assert.Equal(t, Repeat(words: "*", num: 8).repeat, "********", "5")
-	assert.Equal(t, Repeat(words: "abc", num: -2).repeat, "", "6")
+	assert.Equal(t, Repeat{words: "*", num: 3}.repeat(), "***", "1")
+	assert.Equal(t, Repeat{words: "abc", num: 3}.repeat(), "abcabcabc", "2")
+	assert.Equal(t, Repeat{words: "abc", num: 4}.repeat(), "abcabcabcabc", "3")
+	assert.Equal(t, Repeat{words: "abc", num: 1}.repeat(), "abc", "4")
+	assert.Equal(t, Repeat{words: "*", num: 8}.repeat(), "********", "5")
+	assert.Equal(t, Repeat{words: "abc", num: -2}.repeat(), "", "6")
 }
