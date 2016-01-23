88c687868d1628f615298c759338763da728234c
TakaakiFuruse <takaakifuruse@gmail.com>
2016-01-23 15:20:04 +0900

    done

--- freeCodeCampGo/confirmEnding/confEnd.go
+++ freeCodeCampGo/confirmEnding/confEnd.go
@@ -6,14 +6,14 @@ func end(s ...string) bool {
 	wordsToBeChecked := s[0]
 	wordToCheck := s[1]
 	wordsAr := strings.Split(wordsToBeChecked, " ")
-	endLetter := wordsAr[len(wordsAr)-1][len(wordsAr[len(wordsAr)-1])-1]
-	// endLetter := []byte{}
+	lasLetterOfWord := strings.Split(wordsAr[len(wordsAr)-1], "")
+	endLetter := lasLetterOfWord[len(lasLetterOfWord)-1]
 
-	// if len(wordsAr) == 1 && endLetter == wordToCheck {
-	// 	return true
-	// }
+	if len(wordsAr) == 1 && endLetter == wordToCheck {
+		return true
+	}
 
-	if len(wordToCheck) > 1 && endLetter == wordToCheck[1] {
+	if len(wordToCheck) > 1 && endLetter == wordToCheck {
 		return true
 	}
 	if wordsAr[len(wordsAr)-1] == wordToCheck {
